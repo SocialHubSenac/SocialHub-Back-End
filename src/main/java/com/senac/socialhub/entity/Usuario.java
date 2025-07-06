b1bf23cafa207d75d557758bb34db79e9a024d11
ErickMonteiroMDK <erickmonteiromdk@gmail.com>
2025-07-05 21:55:44 -0300

    feat: correção da entidade usuário

--- src/main/java/com/senac/socialhub/entity/Usuario.java
+++ src/main/java/com/senac/socialhub/entity/Usuario.java
@@ -16,7 +16,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class Usuario implements UserDetails {
+public class Usuario {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -35,60 +35,8 @@ public class Usuario implements UserDetails {
     @Column(nullable = false)
     private Role role;
 
-    // ========= Métodos obrigatórios do Spring Security ==========
 
-<<<<<<< HEAD
     @Column(nullable = true)
     private String descricao;
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
-    }
-
-    @Override
-    public String getPassword() {
-        return senha;
-=======
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority("ROLE_" + this.role.name()));
->>>>>>> 79e49255acb80c92c17035389f12ca62d88dd765
-    }
-
-    @Override
-    public String getUsername() {
-<<<<<<< HEAD
-        return email;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
 }
-=======
-        return this.email;
-    }
-
-    @Override
-    public String getPassword() {
-        return this.senha;
-    }
-}
->>>>>>> 79e49255acb80c92c17035389f12ca62d88dd765
